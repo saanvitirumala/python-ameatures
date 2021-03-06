7d2cb11865d54c11908ff6e2ff48bd4a7f964167
saanvitirumala <saanvi.tirumala@gmail.com>
2021-03-06 12:00:54 -0500

    Day 1 course

---
+++ Assignments/Python-course/day-1.py
@@ -0,0 +1,37 @@
+#Practice 1
+saanvi_age =  13
+saharsh_age = 9
+oscar_age = 5
+
+sum_of_ages = saanvi_age + saharsh_age + oscar_age
+
+print("Total age of :",sum_of_ages)
+
+#practice 2
+saharsh_first_name = "saharsh"
+saharsh_last_name = "tirumala"
+
+saharsh_full_name = saharsh_first_name+" "+ saharsh_last_name
+print("Saharsh's full name : ",saharsh_full_name)
+
+
+#Practice 3
+saanvi_bottle = "Apple juice"
+saharsh_bottle = "gingerale"
+
+print("---Beverage in variables---")
+
+print("saanvi_bottle - "+saanvi_bottle)
+print("saharsh_bottle - "+saharsh_bottle)
+
+temp_bottle = saanvi_bottle 
+
+saanvi_bottle = saharsh_bottle 
+
+saharsh_bottle = temp_bottle
+
+print("---Beverage after exchange---")
+
+print("saanvi_bottle- "+saanvi_bottle)
+print("saharsh_bottle - "+saharsh_bottle)
+
