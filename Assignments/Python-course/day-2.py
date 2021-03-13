e413a51081736c76376560dca523a6e61f0e205d
saanvitirumala <saanvi.tirumala@gmail.com>
2021-03-13 18:27:12 -0500

    commit day 2

---
+++ Assignments/Python-course/day-2.py
@@ -0,0 +1,21 @@
+
+#Practice 1
+your_age = int(input("Enter you age : "))
+
+if your_age >= 18 :
+ print("Adult")
+elif your_age > 5 :
+  print("Kid")
+else :
+  print("Baby")
+
+  #practice 2
+saanvi_school_name = "Iroqious Middle School"
+sannvi_age = 13
+
+print("saanvi_school_name:",type(saanvi_school_name))
+oscar_age = 5
+
+print("initial age-",type(oscar_age))
+
+oscar_age = ()
