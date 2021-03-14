21d11fa0b987db651c7fe42567c122215262e799
saanvitirumala <saanvi.tirumala@gmail.com>
2021-03-14 11:19:58 -0400

    Functions

---
+++ Assignments/functions.py
@@ -0,0 +1,29 @@
+def my_function():
+    print(" HELLO WORLD!")
+    
+my_function()
+
+
+def my_function(firstname):
+    print(firstname +" "+ lstname )
+
+lstname = "Tirumala"
+
+my_function("Saanvi")
+my_function("Saharsh")
+
+def my_function(*Item):
+    print("The cheapest item is:"+Item[1])
+
+milk = 1.99
+banana = 3.00
+chips = 9.00
+
+my_function("banana","milk","chips")
+
+
+def my_function(*Fruits):
+  print("The smallest fruit is:"+ Fruits[1])
+
+my_function("apples","grape","Pinneapple")
+
